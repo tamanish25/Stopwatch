637c065097c4f0a1309d59644fe095fb723dd49b
manish25 <tamanish25@gmail.com>
2022-08-05 15:02:44 +0200

    start button

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var stop=document.getElementById('stop');
 var reset=document.getElementById('reset');
 var sec = 0;
 var millisec =0;
+var timeInterval;
 const timer=()=>{
     millisec++;
 
@@ -19,6 +20,16 @@ const timer=()=>{
         seconds.innerHTML ="0"+sec;
         millisec=0;
         milliseconds.innerHTML="0"+0;
-        
+
+    }
+    if (sec>9){
+        seconds.innerHTML=sec;
     }
 }
+
+//Start button
+start.addEventListener('click',()=>{
+    timeInterval=setInterval(timer,10);
+})
+
+//Stop button
